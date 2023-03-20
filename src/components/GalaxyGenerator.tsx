47d453bb835fb88481b4fe85d2bfbb21d6ce36b0
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-03-20 14:55:50 +0600

    install dependency, remove unnecessary file,& setUp basic folder structure

---
+++ src/components/GalaxyGenerator.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GalaxyGenerator = () => {
+  return (
+    <section>
+      <canvas className='webgl'></canvas>
+    </section>
+  )
+}
+
+export default GalaxyGenerator
