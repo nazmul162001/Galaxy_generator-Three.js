96efad1eb1b18ceeac5fdbb59eb705adabad2574
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-03-20 19:07:05 +0600

    added galaxy & added debug

--- src/components/GalaxyGenerator.tsx
+++ src/components/GalaxyGenerator.tsx
@@ -9,7 +9,7 @@ const GalaxyGenerator = () => {
      * Base
      */
     // Debug
-    const gui = new dat.GUI()
+    const gui = new dat.GUI({ width: 360 })
 
     // Canvas
     const canvas: HTMLElement = document.querySelector(
@@ -20,13 +20,90 @@ const GalaxyGenerator = () => {
     const scene = new THREE.Scene()
 
     /**
+     * Galaxy
+     */
+    const parameters = {}
+    //@ts-ignore
+    parameters.count = 1000
+    //@ts-ignore
+
+    let geometry = null
+    let material = null
+    let points = null
+
+    //@ts-ignore
+    parameters.size = 0.02
+
+    const generateGalaxy = () => {
+      /**
+       * Destroy Galaxy
+       */
+      //@ts-ignore
+      if (points !== null) {
+        //@ts-ignore
+        geometry.dispose()
+        //@ts-ignore
+        material.dispose()
+        //@ts-ignore
+        scene.remove(points)
+      }
+
+      /**
+       * Geometry
+       */
+      geometry = new THREE.BufferGeometry()
+      //@ts-ignore
+      const positions = new Float32Array(parameters.count * 3)
+      //@ts-ignore
+      for (let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3
+
+        positions[i3] = (Math.random() - 0.5) * 3
+        positions[i3 + 1] = (Math.random() - 0.5) * 3
+        positions[i3 + 2] = (Math.random() - 0.5) * 3
+      }
+
+      geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+      /**
+       * Material
+       */
+      material = new THREE.PointsMaterial({
+        //@ts-ignore
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+      })
+      /**
+       * Points
+       */
+      points = new THREE.Points(geometry, material)
+      scene.add(points)
+    }
+    generateGalaxy()
+
+    // Debug
+    gui
+      .add(parameters, 'count')
+      .min(100)
+      .max(1000000)
+      .step(100)
+      .onFinishChange(generateGalaxy)
+    gui
+      .add(parameters, 'size')
+      .min(0.001)
+      .max(0.1)
+      .step(0.001)
+      .onFinishChange(generateGalaxy)
+
+    /**
      * Test cube
      */
-    const cube = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial()
-    )
-    scene.add(cube)
+    // const cube = new THREE.Mesh(
+    //   new THREE.BoxGeometry(1, 1, 1),
+    //   new THREE.MeshBasicMaterial()
+    // )
+    // scene.add(cube)
 
     /**
      * Sizes
